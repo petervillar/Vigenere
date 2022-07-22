94838d3e67a67c40c7dd6f64ac1363ebbdfc8757
Peter Villar <peter.villar@gmail.com>
2022-07-22 23:43:42 +0200

    __str__ method

--- vigenere.py
+++ vigenere.py
@@ -1,26 +1,39 @@
 # -*- coding: utf-8 -*-
 
-class vigenereDecoder:
+class vigenereEncoderDecoder:
     """
-    This class decodes a Vigenère-encoded message.
+    This class encodes or decodes using the Vigenère-cipher.
     """
-    def __init__(self, encrypted_msg, keyword):
-        self.encrypted_msg = encrypted_msg
-        self.keyword = keyword
+    def __init__(self, message, key):
+        self.message = message
+        self.key = key
+
+    def __str__(self):
+        return f'vigenereEncoderDecoder({self.message}, {self.key})'
+
+    def encode_message(self):
+        pass
 
     def decode_message(self):
-        for i in range(len(self.encrypted_msg)):
-            N = ord(self.encrypted_msg[i]) - 97
-            K = ord(self.keyword[i%len(self.keyword)]) - 97
+        decoded_msg = []
+
+        for i in range(len(self.message)):
+            N = ord(self.message[i]) - 97 # ord('a') = 97
+            K = ord(self.key[i%len(self.key)]) - 97
             D = N - K
             if D < 0:
                 D = D + 26
 
-            print(i, self.encrypted_msg[i], self.keyword[i%len(self.keyword)], chr(D+97))
+            decoded_msg.append(chr(D+97))
 
-encrypted_msg = input("Enter the encrypted message: ").lower()
-keyword = input("Enter the cipher keyword: ").lower()
+        return ''.join(decoded_msg)
 
-decoder = vigenereDecoder(encrypted_msg, keyword)
-decoded_msg = decoder.decode_message()
+
+message = input("Enter the encrypted message: ").lower()
+key = input("Enter the cipher keyword: ").lower()
+
+encoder_decoder = vigenereEncoderDecoder(message, key)
+print(encoder_decoder)
+
+decoded_msg = encoder_decoder.decode_message()
 print(decoded_msg)
